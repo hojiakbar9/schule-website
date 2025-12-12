1269c5ff94d5b88252547f0d5a0d457ca46de91e
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2025-12-12 20:33:56 +0100

    feat: add TextBox component with mission statement and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import Hero from "./components/Hero";
 import Links from "./components/Link";
 import NavBar from "./components/NavBar";
+import TextBox from "./components/TextBox";
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Hero />
-    </>
-  );
+  return <TextBox></TextBox>;
 }
 
 export default App;
---
+++ src/components/TextBox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const TextBox = () => {
+  return (
+    <div className="flex flex-col w-screen font-accent items-center text-center">
+      <p className="text-accent  text-sm uppercase font-bold mb-2 tracking-widest">
+        Unsere Mission
+      </p>
+      <h2 className="text-3xl md:text-5xl font-body font-bold text-primary mb-6">
+        Wo Tradition Innovation trifft
+      </h2>
+      <p className="text-secondary text-lg leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga,
+        cupiditate labore sint dolorem esse hic exercitationem, sequi veritatis
+        libero accusantium recusandae commodi adipisci! Blanditiis temporibus
+        dolor veniam perspiciatis ipsum neque repellat ad explicabo, eius
+        aperiam reiciendis nobis eum animi doloremque sint. Nesciunt, veritatis
+        tempora? Itaque, pariatur! Blanditiis, quos rerum adipisci quibusdam
+        ullam velit quis exercitationem? Soluta incidunt itaque, sequi veniam
+        architecto ipsum quaerat amet necessitatibus ipsam, iusto eaque at
+        voluptatem enim odit velit praesentium officia perspiciatis magnam! Sit
+        earum non atque ipsum vero repellat enim explicabo perferendis
+        temporibus distinctio in est possimus maxime asperiores ex
+        necessitatibus, voluptates magnam facere culpa!
+      </p>
+    </div>
+  );
+};
+
+export default TextBox;
